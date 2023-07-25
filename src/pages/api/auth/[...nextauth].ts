3543036e4086779095f0f64455a5cefb39b7b21b
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-07-25 22:03:37 +0700

    test adapter

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import { prisma } from '@/libs/db';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+// import { Adapter } from 'next-auth/adapters';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, user }: { session: any; user: any }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // pages: {
+  //   signIn: '/login',
+  // },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
