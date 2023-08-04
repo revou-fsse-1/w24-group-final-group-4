7206a081a08b23c61bc04015915f5c66331d6f1a
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-08-04 22:12:27 +0700

    fix detail

--- src/components/DetailPostCard.tsx
+++ src/components/DetailPostCard.tsx
@@ -27,10 +27,10 @@ function DetailPostCard({
       <div>
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-3">
-            <div className="rounded-full flex justify-center items-center bg-gray-400 p-2 aspect-square min-h-[3rem] overflow-hidden ">
+            <div className="rounded-full flex justify-center items-center bg-gray-400 aspect-square min-h-[3rem] overflow-hidden ">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                className="w-full h-full scale-150"
+                // className="w-full h-full scale-150"
                 src={createAvatar(thumbs, {
                   seed: post.user.name as string,
                 }).toDataUriSync()}
